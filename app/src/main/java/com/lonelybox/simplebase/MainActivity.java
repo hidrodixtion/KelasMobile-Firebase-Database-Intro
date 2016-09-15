811cd106b7986c21d974010340d44a23d53ba861
Adi Nugroho <hidrodixtion@gmail.com>
2016-09-15 16:12:01 +0700

    Added firebase code.

--- app/src/main/java/com/lonelybox/simplebase/MainActivity.java
+++ app/src/main/java/com/lonelybox/simplebase/MainActivity.java
@@ -2,12 +2,56 @@ package com.lonelybox.simplebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtTitle;
+    private EditText editTitle;
+    private Button btnSync;
+    private DatabaseReference mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTitle = (TextView) findViewById(R.id.txt_title);
+        editTitle = (EditText) findViewById(R.id.edit_title);
+        btnSync = (Button) findViewById(R.id.btn_sync);
+
+        initFirebase();
+
+        btnSync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFirebaseRef.child("title").setValue(editTitle.getText().toString());
+            }
+        });
+    }
+
+    private void initFirebase() {
+        mFirebaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mFirebaseRef.child("title").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                txtTitle.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
